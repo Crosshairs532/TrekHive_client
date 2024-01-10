4231ee3924432055d773290abc47d5a2a93a3cfe
Md.Samsul Arefin <samsularefin532@gmail.com>
2024-01-11 00:41:04 +0600

    tour types done

--- src/Components/AllTourTypes.jsx
+++ src/Components/AllTourTypes.jsx
@@ -1,12 +1,47 @@
+import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
+import AxiosPublic from "../Axios/AxiosPublic";
+import Loaading from "../Loading/Loaading";
+import Cards from "./Cards";
 
 
 const AllTourTypes = () => {
     const { type } = useParams();
+    console.log(type)
+    const axiosPublic = AxiosPublic();
+    const { data: allTours = [], isFetched } = useQuery({
+        queryKey: ['specific_type_tour'],
+        queryFn: async () => {
+            const res = await axiosPublic.get(`/packages?tour=${type}`)
+            return res.data;
+        }
 
+    })
+    if (!isFetched) {
+        return <Loaading></Loaading>
+    }
     return (
-        <div>
+        <div className="pt-[114px] pb-[20px]">
+            {
+                allTours.length > 0 ? (
+                    <div className=" grid  lg:grid-cols-3 gap-2 md:grid-cols-2 grid-cols-1 place-items-center">
+                        {
+                            allTours.map((item, idx) => (
+                                <Cards key={idx} id={item._id} image={item.image} title={item.title} tourType={item.tourType} price={item.price}></Cards>
+                            ))
+                        }
 
+                    </div>
+
+                ) : <>
+
+                    <div className=" flex no_package justify-center items-center h-[calc(100vh-114px)] w-full ">
+                        <h1 className=" text-gray-400 text-5xl font-syne font-bold"> No package Available!</h1>
+                    </div>
+
+
+                </>
+            }
         </div>
     );
 };
--- src/Components/TourTypes.jsx
+++ src/Components/TourTypes.jsx
@@ -13,7 +13,7 @@ const TourTypes = () => {
             <h1 className=" font-syne guide overflow-hidden relative text-xl lg:text-4xl font-bold ">Types of Tour we Offer</h1>
             <div className=' mt-[6%] '>
                 <div className=" grid lg:grid-cols-4 gap-y-2 md:grid-cols-2 grid-cols-1 place-items-center">
-                    <Link to={'/'}>
+                    <Link to={'/tourtypes/hiking'}>
                         <div className=" tour-type cursor-pointer group relative overflow-hidden w-[300px] h-[400px] ">
                             <img className=' group-hover:scale-125 duration-500 w-full h-[400px] object-cover' src={hiking} alt="" />
                             <div className=" px-2 flex text-[#e4e4e4] font-syne items-center justify-between z-10 bg-[#ffffff0e] backdrop-blur-[7px] w-full h-[70px] bottom-0 left-0 absolute">
@@ -22,7 +22,7 @@ const TourTypes = () => {
                             </div>
                         </div>
                     </Link>
-                    <Link>
+                    <Link to={'/tourtypes/air'}>
                         <div className=" tour-type group cursor-pointer  overflow-hidden w-[300px] h-[400px] relative">
                             <img className=' group-hover:scale-125 duration-500 w-full h-[400px] object-cover' src={air} alt="" />
                             <div className=" px-2 flex text-[#e4e4e4] font-syne items-center justify-between z-10 bg-[#ffffff0e] backdrop-blur-[7px] w-full h-[70px] bottom-0 left-0 absolute">
@@ -30,7 +30,7 @@ const TourTypes = () => {
                                 <button className=' outline px-1 py-2 outline-blue-gray-100'>view more</button>
                             </div>
                         </div></Link>
-                    <Link>
+                    <Link to='/tourtypes/wildlife'>
                         <div className=" tour-type group cursor-pointer  overflow-hidden w-[300px] h-[400px] relative">
                             <img className=' group-hover:scale-125 duration-500 w-full h-[400px] object-cover' src={wildlife} alt="" />
                             <div className=" px-2 flex text-[#e4e4e4] font-syne items-center justify-between z-10 bg-[#ffffff0e] backdrop-blur-[7px] w-full h-[70px] bottom-0 left-0 absolute">
@@ -39,7 +39,7 @@ const TourTypes = () => {
                             </div>
                         </div>
                     </Link>
-                    <Link>
+                    <Link to={'/tourtypes/sports'}>
 
                         <div className=" tour-type group cursor-pointer  overflow-hidden w-[300px] h-[400px] relative">
                             <img className=' group-hover:scale-125 duration-500 w-full h-[400px] object-cover' src={sports} alt="" />
